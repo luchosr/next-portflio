57c3e060968081be5a6b774a1bfbc44cccc1eb9c
Luciano Ramello <luchosr@gmail.com>
2024-09-27 17:17:30 +0200

    feat: updates layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
+import { Inter, Playfair_Display } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+
 import './globals.css'
 import Providers from '@/components/Providers'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900'
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900'
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+const playfair = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-serif'
 })
 
 export const metadata: Metadata = {
@@ -27,9 +27,17 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          'flex min-h-screen flex-col font-sans antialiased',
+          inter.variable,
+          playfair.variable
+        )}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          <Header />
+          <main className='mx-auto grow'>{children}</main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   )
