f3400f0233f43c951876c7b50814d964c42810e4
Luciano Ramello <luchosr@gmail.com>
2024-09-27 18:10:35 +0200

    feat: implements MDXRemote on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Intro from '@/components/Intro'
+import { MDXRemote } from 'next-mdx-remote/rsc'
 import React from 'react'
 
 export default function Home() {
+  const content = `# This is a markdown heading`
   return (
     <section className='py-24'>
       <div className='container max-w-3xl'>
         <Intro />
+        <MDXRemote source={content} />
       </div>
     </section>
   )
