53f1ff72a0abd90643da19796d291f3248eb48f4
Luciano Ramello <luchosr@gmail.com>
2024-09-30 18:02:55 +0200

    feat: adds PostsPage component

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import { getPosts } from '@/lib/posts'
+// import PostsWithSearch from '@/components/posts-with-search'
 
-export default function PostsPage() {
-  return <div>PostsPage</div>
+import Posts from '@/components/Posts'
+
+export default async function PostsPage() {
+  const posts = await getPosts()
+
+  return (
+    <section className='pb-24 pt-40'>
+      <div className='container max-w-3xl'>
+        <h1 className='title mb-12'>Posts</h1>
+
+        <Posts posts={posts} />
+      </div>
+    </section>
+  )
 }
