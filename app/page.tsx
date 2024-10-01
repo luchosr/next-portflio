2ab4b3dbe637f3639cd744719c5e2e6cfa7ac394
Luciano Ramello <luchosr@gmail.com>
2024-10-01 19:35:15 +0200

    feat: implemented RectPost on app's layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Intro from '@/components/Intro'
-import { MDXRemote } from 'next-mdx-remote/rsc'
+import RecentPosts from '@/components/RecentPosts'
 import React from 'react'
 
 export default function Home() {
-  const content = `# This is a markdown heading`
   return (
     <section className='py-24'>
       <div className='container max-w-3xl'>
         <Intro />
-        <MDXRemote source={content} />
+        <RecentPosts />
       </div>
     </section>
   )
